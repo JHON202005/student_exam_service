9d786c67293b55c5e48b75e52aeed961d734aa22
JHON <alimpongjohnernie@gmail.com>
2026-05-20 16:37:32 +0800

    add predict view

--- backend/server/apps/endpoints/tests.py
+++ backend/server/apps/endpoints/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+class EndpointTests(TestCase):
+
+    def test_predict_view(self):
+        client = APIClient()
+        input_data = {
+            "gender": "male",
+            "race/ethnicity": "group B",
+            "parental level of education": "bachelor's degree",
+            "lunch": "standard",
+            "test preparation course": "none",
+            "reading score": 72,
+            "writing score": 74
+        }
+        classifier_url = "/api/v1/exam_score_classifier/predict"
+        response = client.post(classifier_url, input_data, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(response.data["label"], ["Pass", "Fail"])
+        self.assertTrue("request_id" in response.data)
+        self.assertTrue("status" in response.data)
